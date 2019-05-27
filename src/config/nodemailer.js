3212db911bf2fb3f0f381c0f51a3595ba589fcc3
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-05-27 23:14:29 +0300

    added sendgrid

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,12 +1,12 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 const Email = require('email-templates');
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.GMAIL_EMAIL,
-    pass: process.env.GMAIL_PASS
-  }
-});
+const transporter = nodemailer.createTransport(
+  sgTransport({
+    port: 465,
+    auth: { api_key: process.env.SENDGRID_API_KEY }
+  })
+);
 
 module.exports = new Email({
   transport: transporter,
